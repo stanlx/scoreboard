23be5ac3525cd56f1be1a052b7410aa6eee52510
stanlx <stanmelamed@gmail.com>
2012-10-14 14:31:56 +0000

    s

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoreboard::Application.config.secret_token = 'a298f9170e22e8e7d93502381add2d5aad9a5a89e7b1b6e03d60f78e26de7bdf45ecb2eebc0c2d889d2e4ce2d24be1746223492cdd17d72438264af5b8ebd062'
